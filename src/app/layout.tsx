4adcf5afb3c47747c0c2b02e2822f774f3eb938f
Rohith Gilla <gillarohith1@gmail.com>
2024-08-30 21:20:50 +0530

    chore: Add clarity and simple ananlytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Navbar } from "@/components/navbar";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,16 @@ export default function RootLayout({
           <main className="container mx-auto py-6">{children}</main>
         </ThemeProvider>
       </body>
+      <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
+      <Script id="microsoft-clarity-analytics">
+        {`
+      (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "nvg48rkzuo");
+      `}
+      </Script>
     </html>
   );
 }
